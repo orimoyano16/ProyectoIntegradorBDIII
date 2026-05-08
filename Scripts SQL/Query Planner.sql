120d9ea0c812c9b8c0443098c0a571d857f0f975
Santiago Marranti <marrantis@gmail.com>
2026-05-08 00:57:51 -0300

    Agrego indexacion, actualizo Query Planner

--- Scripts SQL/Query Planner.sql
+++ Scripts SQL/Query Planner.sql
@@ -1,11 +1,6 @@
-
--- Analizamos sin indexacion
 EXPLAIN (ANALYZE, VERBOSE)
 SELECT * from usuario WHERE email = 'email120453@gmail.com';
 
--- Creamos el indice 
-create index idx_usuario_email on usuario using hash(email);
-
--- analizamos nuevamente los tiempos y costos de la busqueda
-EXPLAIN (ANALYZE, VERBOSE)
+-- Dalibo
+EXPLAIN (ANALYZE, COSTS, VERBOSE, BUFFERS, FORMAT JSON)
 SELECT * from usuario WHERE email = 'email120453@gmail.com';
