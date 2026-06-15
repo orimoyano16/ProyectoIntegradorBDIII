a5acb6198e94e1d9f3a862d88531c7cff79fea01
aariadnavs <ariadnavsantillan@gmail.com>
2026-06-15 19:03:41 -0300

    Agrego ENDPOINT Delete para dar de baja a usuarios

--- Parte 3 - Redis/backend/src/routes/usuario.routes.js
+++ Parte 3 - Redis/backend/src/routes/usuario.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { cacheAside } = require('../cache/cacheAside');
 const keys = require('../cache/keys');
-const { getUsuarioByDni } = require('../services/usuario.service');
+const { getUsuarioByDni, bajaDeUsuario } = require('../services/usuario.service');
+const { redis, isRedisAvailable } = require('../config/redis');
 
 const router = express.Router();
 
@@ -25,5 +26,27 @@ router.get('/usuarios/dni/:dni', async (req, res, next) => {
     next(err);
   }
 });
+
+router.delete('/usuarios/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const usuarioDadoDeBaja = await bajaDeUsuario(id);
+
+    if (isRedisAvailable()) {
+      const cacheKey = keys.usuarioDni(usuarioDadoDeBaja.dni);
+      await redis.del(cacheKey);
+      console.log(`[Cache] DEL → ${cacheKey}`);
+    }
+
+    res.json({
+      mensaje: 'Usuario dado de baja correctamente',
+      usuario: usuarioDadoDeBaja,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
 
 module.exports = router;
--- Parte 3 - Redis/backend/src/services/usuario.service.js
+++ Parte 3 - Redis/backend/src/services/usuario.service.js
@@ -36,4 +36,45 @@ async function getUsuarioByDni(dni) {
   };
 }
 
-module.exports = { getUsuarioByDni };
+// Agrego nueva función: DELETE para dar de baja lógica
+async function bajaDeUsuario(id) {
+  const { rows: existing } =  await pool.query(
+    'SELECT usuario_id, dni, fecha_baja FROM usuario WHERE usuario_id = $1',
+    [id]
+  );
+
+  if (existing.length === 0){
+    const error = new Error(`No se encontró usuario con ID ${id}`);
+    error.statusCode = 400;
+    throw error;
+  }
+
+  if (existing[0].fecha_baja !== null){
+    const error = new Error(`El usuario con ID ${id} ya fue dado de baja`);
+    error.statusCode = 409;
+    throw error;
+  }
+
+  const { rows } = await pool.query (
+    `UPDATE usuario
+    SET fecha_baja = now()
+    WHERE usuario_id = $1
+    RETURNING usuario_id, nombre, apellido, email, dni, rol, fecha_baja`,
+    [id]
+  );
+
+  const u = rows[0];
+  return {
+    usuarioId: u.usuario_id,
+    nombre: u.nombre,
+    apellido: u.apellido,
+    email: u.email,
+    dni: Number(u.dni),
+    rol: u.rol,
+    fechaBaja: u.fecha_baja,
+  };
+}
+
+
+
+module.exports = { getUsuarioByDni, bajaDeUsuario };
